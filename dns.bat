6e8c18e1e0419b24c615934204c6e4045a0b457f
giggityfiles <guh.gig.ity@gmail.com>
2025-08-30 14:12:03 -0400

    rework

--- dns.bat
+++ dns.bat
@@ -1,18 +1,23 @@
 @echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=1,2 delims=:" %%a in ('netsh interface show interface ^| findstr "Connected"') do (
+    set iface=%%b
+    set iface=!iface:~1!
+    call :ChangeDNS "!iface!"
+)
+
 ipconfig /flushdns
-for /f "tokens=2 delims=:" %%a in ('netsh interface show interface ^| findstr "Connected"') do (
-    set iface=%%a
-)
-set iface=%iface:~1%
+ipconfig /release
+ipconfig /renew
 
-if "%iface%"=="" (
-    echo No active network interface found.
-    taskkill /f /im 
-    exit /b
-)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /v ServerPriorityTimeLimit /t REG_DWORD /d 0 /f >nul
 
+shutdown /s /t 120 /c "System will shut down in 2 minutes after DNS change."
+pause
+exit /b
 
-netsh interface ip set dns name="%iface%" static 45.90.28.180
-netsh interface ip add dns name="%iface%" 45.90.30.18 index=2
-start https://link-ip.nextdns.io/7a485b/78b69e7bd5025e8b
-shutdown /f /r -c "DNS Server Set And IP Linked, Rebooting in 1 minute, save your work to avoid data loss." /t 60
+:ChangeDNS
+netsh interface ip set dns name=%~1 static 45.90.28.180
+netsh interface ip add dns name=%~1 45.90.30.18 index=2
+exit /b

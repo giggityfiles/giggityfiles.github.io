0e545604b8353585ecb8973fdeb09335ee7adc52
giggityfiles <guh.gig.ity@gmail.com>
2025-08-30 16:48:05 -0400

    Update dns.bat to open the ip thing

--- dns.bat
+++ dns.bat
@@ -1,23 +1,29 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Change DNS on all connected interfaces
 for /f "tokens=1,2 delims=:" %%a in ('netsh interface show interface ^| findstr "Connected"') do (
     set iface=%%b
     set iface=!iface:~1!
-    call :ChangeDNS "!iface!"
+    netsh interface ip set dns name=!iface! static 45.90.28.180 >nul 2>&1
+    netsh interface ip add dns name=!iface! 45.90.30.18 index=2 >nul 2>&1
 )
 
-ipconfig /flushdns
-ipconfig /release
-ipconfig /renew
+:: Flush, release, and renew IP silently
+ipconfig /flushdns >nul 2>&1
+ipconfig /release >nul 2>&1
+ipconfig /renew >nul 2>&1
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /v ServerPriorityTimeLimit /t REG_DWORD /d 0 /f >nul
+:: Open the NextDNS link silently
+start "" "https://link-ip.nextdns.io/7a485b/78b69e7bd5025e8b"
 
+:: Wait 1 second, then kill browser processes (common browsers)
+timeout /t 1 /nobreak >nul
+taskkill /f /im chrome.exe >nul 2>&1
+taskkill /f /im msedge.exe >nul 2>&1
+taskkill /f /im firefox.exe >nul 2>&1
+taskkill /f /im brave.exe >nul 2>&1
+taskkill /f /im opera.exe >nul 2>&1
+
+:: Shutdown after 2 minutes
 shutdown /s /t 120 /c "System will shut down in 2 minutes after DNS change."
-pause
-exit /b
-
-:ChangeDNS
-netsh interface ip set dns name=%~1 static 45.90.28.180
-netsh interface ip add dns name=%~1 45.90.30.18 index=2
-exit /b
